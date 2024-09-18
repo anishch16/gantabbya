a29615579ed441176cf497f713d0579e5e22cee2
chaulagainanish16 <anish.chaulagain@ankaek.com>
2024-09-19 01:38:19 +0545

    changed home page design

--- lib/app/utils/greetings.dart
+++ lib/app/utils/greetings.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 String getGreetingMessage() {
   // Get the current time
   final DateTime now = DateTime.now();
@@ -12,5 +14,38 @@ String getGreetingMessage() {
     return "Good evening";
   } else {
     return "Good night";
+  }
+}
+IconData getGreetingIcon() {
+  // Get the current time
+  final DateTime now = DateTime.now();
+  final int hour = now.hour;
+
+  // Define icon based on the current hour
+  if (hour >= 5 && hour < 12) {
+    return Icons.wb_sunny; // Morning icon
+  } else if (hour >= 12 && hour < 17) {
+    return Icons.wb_cloudy; // Afternoon icon
+  } else if (hour >= 17 && hour < 20) {
+    return Icons.sunny_snowing; // Evening icon (optional)
+  } else {
+    return Icons.nights_stay; // Night icon
+  }
+}
+
+Color getGreetingColor() {
+  // Get the current time
+  final DateTime now = DateTime.now();
+  final int hour = now.hour;
+
+  // Define color based on the current hour
+  if (hour >= 5 && hour < 12) {
+    return Colors.orangeAccent; // Morning color
+  } else if (hour >= 12 && hour < 17) {
+    return Colors.blueAccent; // Afternoon color
+  } else if (hour >= 17 && hour < 20) {
+    return Colors.purpleAccent; // Evening color
+  } else {
+    return Colors.indigo; // Night color
   }
 }
